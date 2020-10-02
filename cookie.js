1d3c35c13b54c23e466f7cc57b18b5790241fbca
Kaishya <desai.kaishya@gmail.com>
2020-10-02 09:01:57 -0600

    working doubler

--- cookie.js
+++ cookie.js
@@ -1,48 +1,52 @@
 console.log('start')
 
+//I honestly did not get the challenge done, but I do have 
+//the working doubler code
+
+
 let cookieEl = document.getElementById('cookie')
-let clickEl = document.getElementById('click')
+//let clickEl = document.getElementById('cookie')
 let clickCount = 0
 let scoreEl = document.getElementById('score')
 let doublerButton = document.getElementById('doubler')
+let scoreChange = 1
+let haveDoubled = false
+
 let countChange = 1
 
+
+
 function cookieClicked() {
-    console.log('was clicked')
+    
     clickCount = clickCount + countChange
     scoreEl.innerHTML = clickCount
+    if (clickCount === 10) {
+        if(!haveDoubled) {
+            doublerButton.classList.remove('remove')
+        }
+    }
+    
 }
+
 cookieEl.addEventListener('click',cookieClicked)
 
 
 ///////doubler
 
-if(clickCount === 10) {
-    if (countChange === 1) {
-        doublerButton.classlist.remove('remove')
-    }
-}
 function buyDoubler() {
-    clickCount = clickCount -10
-
-    scoreEl.innerhtml = clickCount
-    doublerButton.classlist.add('remove')
+    haveDoubled = true
+    clickCount = clickCount - 10
+    
+    scoreEl.innerHTML = clickCount
+    doublerButton.classList.add('remove')
     countChange = 2
 }
 doublerButton.addEventListener('click', buyDoubler)
 
 let showButton = document.getElementById('show')
+console.log(showButton)
 let removeButton = document.getElementById('remove')
 
-
-//////////click animation
-//function shrink() {
-  //  cookieEl.classlist.add('shrink')
-//}
-
-function grow() {
-    cookieEl.classList.remove('shrink')
-}
 
 cookieEl.addEventListener('click', cookieClicked)
 //cookieEl.addEventListener('mousedown', shrink)
@@ -50,27 +54,41 @@ cookieEl.addEventListener('click', grow)
 
 
 function showCookie() {
-    clickEl.classlist.remove('remove')
+    cookieEl.classList.remove('remove')
 
-    showButton.classlist.remove('remove')
-    removeButton.add('remove')
+    showButton.classList.remove('remove')
+    removeButton.classList.add('remove')
 }
 
 function hideCookie() {
-    clickEl.classlist.add('remove')
+    cookieEl.classList.add('remove')
 
-    showButton.classlist.remove('remove')
-    removeButton.add('remove')
+    showButton.classList.remove('remove')
+    removeButton.classList.add('remove')
 }
 
 showButton.addEventListener('click', showCookie)
-//removeButton.addEventListener('click', hideCookie)
-//showButton.classlist.add('remove')
+removeButton.addEventListener('click', hideCookie)
+//SshowButton.classList.add('remove')
 
-
+var num = 0;
+window.onload = function () {
+    var name = prompt("What is your name");
+    
+    var space = document.getElementById("space");
+    space.innerHTML = name + "'s Bakery";
+}
 
 
 console.log('end')
+
+
+
+
+
+
+
+
 
 
 
